a162c0e049b6226e3422f40114b4f13b30b78519
CesarShogun <cesar.echavarren@gmail.com>
2022-09-08 15:15:37 +0100

    First Commit

---
+++ RomanNumerals/Models/AllNumerals.cs
@@ -0,0 +1,27 @@
+﻿namespace RomanNumeralsNamespace.Models
+{
+    public static class AllNumerals
+    {
+        public static readonly List<Numeral> NUMERALS = new()
+            {
+                new Numeral('I', 1, true, true),
+                new Numeral('V', 5, false, false),
+                new Numeral('X', 10, true, true),
+                new Numeral('L', 50, false, false),
+                new Numeral('C', 100, true, true),
+                new Numeral('D', 500, false, false),
+                new Numeral('M', 1000, true, false)
+            };
+
+        public static Numeral GetNumeral(char letter)
+        {
+            for (int i = 0; i < NUMERALS.Count; i++)
+            {
+                if (NUMERALS[i].Letter == letter)
+                    return NUMERALS[i];
+            }
+
+            throw new ArgumentException($"NUMERAL ERROR: The \"{letter}\" numeral does not exist.");
+        }
+    }
+}
---
+++ RomanNumerals/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RomanNumeralsNamespace.Models;
+
+Console.WriteLine("Please insert your roman numeral in all caps: ");
+string ? userNumber = Console.ReadLine();
+if (userNumber == null) userNumber = "";
+Console.WriteLine("The value of that number is: " + RomanNumerals.NumeralToInt(userNumber));
+
+/*Console.WriteLine(RomanNumerals.NumeralToInt("I"));
+Console.WriteLine(RomanNumerals.NumeralToInt("II"));
+Console.WriteLine(RomanNumerals.NumeralToInt("III"));
+Console.WriteLine(RomanNumerals.NumeralToInt("IV"));
+Console.WriteLine(RomanNumerals.NumeralToInt("V"));
+Console.WriteLine(RomanNumerals.NumeralToInt("VI"));
+Console.WriteLine(RomanNumerals.NumeralToInt("VII"));
+Console.WriteLine(RomanNumerals.NumeralToInt("VIII"));
+Console.WriteLine(RomanNumerals.NumeralToInt("IX"));
+Console.WriteLine(RomanNumerals.NumeralToInt("X"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XI"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XII"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XIII"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XIV"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XV"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XVI"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XVII"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XVIII"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XIX"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XX"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XXI"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XXII"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XXIII"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XXIV"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XXV"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XXVI"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XXVII"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XXVIII"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XXIX"));
+Console.WriteLine(RomanNumerals.NumeralToInt("XXX"));*/
